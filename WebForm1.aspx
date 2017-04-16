4e1c642196d4bff3dc913de58a5497b9a49afaf5
kptran12 <kris.tran8@gmail.com>
2017-04-16 16:09:44 -0700

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Assignment4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<html> 
+
+   <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" />
+   
+	
+<title> Kristen Tran</title>
+
+
+
+<link href = "~/CSS/bootstrap.min.css" rel= "stylesheet"/>
+<link href= "~/CSS/hangman.css" rel= "stylesheet"/>
+
+</head>
+
+<body> 
+<nav class= "navbar-inverse navbar-fixed-top">
+	<div class = "container">
+	<div class= "navbar-header">
+		<button type = "button" class = "navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria controls = "navbar">
+			
+			<span class = "sr-only"> Toggle Navigation </span>
+			<span class = "icon-bar"></span>
+			<span class = "icon-bar"> </span>
+			<span class = "icon-bar"> </span>
+			<span class = "icon-bar"> </span>
+			</button>
+			<a class = "navbar-brand" href="index.html"> Kristen Tran</a>
+			</div>
+			<div id = "navbar" class = "collapse navbar-collapse">
+				<ul class = "nav navbar-nav">
+					<li class ="active"><a href="#"> Home </a></li>
+					</ul>
+					</div>
+					</div>
+					</nav>
+					
+<body>
+    <form id="form1" runat="server">
+    <div>
+     <div id="hangmanContainer" style="height: 435px; margin-left: 173px; margin-right: 170px; left: 55px; width: 1022px;">
+        <asp:Label ID="answerLb" runat="server" Text="Label"></asp:Label>
+    
+        <asp:Image ID="hangmanImg" runat="server" ImageUrl ="~/Images/hangman.png" />
+        <asp:Label ID="guessNum" runat="server" Text="Number of Guesses:"></asp:Label>
+    
+    
+    </div>
+        <p>
+            <asp:TextBox ID="guessTb" runat="server"></asp:TextBox>
+            <asp:Button ID="fullGuessBtn" runat="server" OnClick="fullGuessBtn_Click" Text="Submit Full Guess" />
+        </p>
+        <asp:TextBox ID="letterGuess" runat="server"></asp:TextBox>
+        <asp:Button ID="letterGuessBtn" runat="server" OnClick="letterGuessBtn_Click" Text="Submit Letter Guess" />
+        <p>
+            <asp:Label ID="guessLb" runat="server" Text="You have guessed..."></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="validLb" runat="server" Text="Will you guess right?"></asp:Label>
+        </p>
+    
+         <asp:RadioButtonList ID="difficultyRB" runat="server" >
+             <asp:ListItem Value ="easyBtn" Selected="True" Text="Easy"></asp:ListItem>
+             <asp:ListItem Value ="medBtn" Text="Medium"></asp:ListItem>
+             <asp:ListItem Value ="hardBtn" Text="Hard"></asp:ListItem>
+         </asp:RadioButtonList>
+
+    
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment4
+{
+
+
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected string Answer
+        {
+            get
+            {
+                if (Session["Answer"] == null)
+                    Session["Answer"] = GetRandomAnswer();
+                return (string)Session["Answer"];
+                string input = String.Empty;
+                char letter;
+                char[] letters = Answer.ToCharArray();
+                char[] display = letters;
+                int i = 0;
+                bool hasRun = false;
+
+                if (hasRun == false)
+                {
+                    foreach (char l in letters)
+                    {
+                        display[i] = '_';
+                        i++;
+                    }
+                    hasRun = true;
+                }
+                answerLb.Text = display.ToString();
+            }
+
+        }
+
+
+
+        protected int Guesses
+        {
+            get
+            {
+
+                if
+                    (Session["Guesses"] == null)
+                    Session["Guesses"] = 0;
+                return (int)Session["Guesses"];
+            }
+            set
+            {
+                Session["Guesses"] = value;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            string input = String.Empty;
+            char letter;
+            char[] letters = Answer.ToCharArray();
+            char[] display = letters;
+            int i = 0;
+            bool hasRun = false;
+
+            if (hasRun == false)
+            {
+                foreach (char l in letters)
+                {
+                    display[i] = '_';
+                    i++;
+                }
+                hasRun = true;
+            }
+            Console.WriteLine("The answer is: " + Answer);
+            answerLb.Text = Answer;
+            
+        }
+
+
+
+        protected void fullGuessBtn_Click(object sender, EventArgs e)
+        {
+
+            guessLb.Text = "You guessed..." + guessTb.Text;
+            if (guessTb.Text.Length > 1)
+            {
+                if (guessTb.Text == Answer)
+                {
+                    validLb.Text = ("You got the answer right!! Congratulations!");
+                }
+                else
+                {
+                    if (difficultyRB.SelectedValue == "easyBtn")
+                    {
+                        int lives = 6;
+                        Guesses = Guesses + 1;
+
+                        if (Guesses == 6)
+                        {
+                            validLb.Text = "Game Over!";
+                            guessNum.Text = "6/6";
+                        }
+                        else
+                        {
+                            hangmanImg.ImageUrl = "~/Images/hangman" + Guesses.ToString() + ".png";
+
+                            guessNum.Text = Guesses.ToString() + "/" + "6";
+
+                            validLb.Text = ("You guessed wrong!");
+                        }
+                    }
+                    if (difficultyRB.SelectedValue == "medBtn")
+                    {
+                        int lives = 4;
+                        Guesses = Guesses + 1;
+
+                        if (Guesses == 4)
+                        {
+                            validLb.Text = "Game Over!";
+                            guessNum.Text = "4/4";
+                        }
+                        else
+                        {
+                            hangmanImg.ImageUrl = "~/Images/hangman" + "M" + Guesses.ToString() + ".png";
+
+                            guessNum.Text = Guesses.ToString() + "/" + "4";
+
+                            validLb.Text = ("You guessed wrong!");
+                        }
+                    }
+
+                    else if (difficultyRB.SelectedValue == "hardBtn")
+                    {
+                        int lives = 2;
+
+                        Guesses = Guesses + 1;
+                        if (Guesses == 2)
+                        {
+                            validLb.Text = "Game Over!";
+                            guessNum.Text = "2/2";
+                        }
+                        else
+                        {
+                            hangmanImg.ImageUrl = "~/Images/hangman" + "H" + Guesses.ToString() + ".png";
+
+                            guessNum.Text = Guesses.ToString() + "/" + "2";
+                        }
+                    }
+                    
+
+                }
+            }
+        }
+
+        protected void letterGuessBtn_Click(object sender, EventArgs e)
+        {
+            string input = String.Empty;
+            char letter;
+            char[] letters = Answer.ToCharArray();
+            char[] display = letters;
+            int i = 0;
+            bool hasRun = false;
+
+            if (hasRun == false)
+            {
+                foreach (char l in letters)
+                {
+                    display[i] = '_';
+                    i++;
+                }
+                hasRun = true;
+            }
+
+
+         
+
+            input = letterGuess.Text;
+            letter = input[0];
+
+            guessLb.Text = ("You guessed..." + letterGuess.Text);
+            if (letters.Contains(letter))
+            {
+                validLb.Text = ("You guessed correctly!");
+
+            }
+            else
+            { 
+                if (difficultyRB.SelectedValue == "easyBtn")
+                {
+                    int lives = 6;
+                    Guesses = Guesses + 1;
+
+                    if (Guesses == 6)
+                    {
+                        validLb.Text = "Game Over!";
+                        guessNum.Text = "6/6";
+                    }
+                    else
+                    {
+                        hangmanImg.ImageUrl = "~/Images/hangman" + Guesses.ToString() + ".png";
+
+                        guessNum.Text = Guesses.ToString() + "/" + "6";
+
+                        validLb.Text = ("You guessed wrong!");
+                    }
+                }
+                if (difficultyRB.SelectedValue == "medBtn")
+                {
+                    int lives = 4;
+
+                    Guesses = Guesses + 1;
+                    if (Guesses == 4)
+                    {
+                        validLb.Text = "Game Over!";
+                        guessNum.Text = "4/4";
+                    }
+                    else
+                    {
+                        hangmanImg.ImageUrl = "~/Images/hangman" + "M" + Guesses.ToString() + ".png";
+
+                        guessNum.Text = Guesses.ToString() + "/" + "4";
+
+                        validLb.Text = ("You guessed wrong!");
+                    }
+                }
+
+                else if (difficultyRB.SelectedValue == "hardBtn")
+                {
+                    int lives = 2;
+                    Guesses = Guesses + 1;
+
+                    if (Guesses == 6)
+                    {
+                        validLb.Text = "Game Over!";
+                        guessNum.Text = "4/4";
+                    }
+                    else
+                    {
+                        hangmanImg.ImageUrl = "~/Images/hangman" + "H" + Guesses.ToString() + ".png";
+
+                        guessNum.Text = Guesses.ToString() + "/" + "2";
+                    }
+                }
+
+
+            }
+        }
+                    
+                
+                    
+        
+               
+            
+        
+
+        
+
+        private string GetRandomAnswer()
+        {
+            string[] answers = {"chocolate", "vanilla", "strawberry", "mango", "pecan","mint","caramel","orange","cherry",
+                        "melon","grape", "coffee", "oreo","peach","raspberry","cinnamon","banana", "pistachio","lemon","cheesecake"};
+            Random rand = new Random();
+            return answers[rand.Next(0, answers.Length)];
+        }
+
+ 
+
+
+
+
+        
+
+        }
+    }
+
+
+
